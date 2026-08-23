a698b6aade773d621d4c9f25498a35130f787a69
same-same-ia <isamodol@gmail.com>
2026-08-23 11:22:21 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 /* IMM izlog service worker: cache-first, fully-offline board.
    The cache name is stamped at build time from a content hash of the built page,
    so every rebuild rolls the cache; activate deletes the old one. */
-var CACHE = 'imm-izlog-13106946dd52';
+var CACHE = 'imm-izlog-14a97bb5f5c1';
 var ASSETS = ['./', './index.html', './manifest.json',
               './icon-192.png', './icon-512.png', './icon-180.png'];
 
